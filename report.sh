0d8a3b19964ca54423f3e25e0d3902378554990a
Ubuntu <ubuntu@ip-172-31-60-28.us-west-2.compute.internal>
2023-07-05 13:18:48 +0000

    update report command

--- report.sh
+++ report.sh
@@ -1,1 +1,1 @@
-find profiling_db -name '*json' -exec cat {} \; | jq -s '[.[] | { command, timestamp, exit_status, elapsed_time_sec }] | sort_by(.timestamp)' |less
+find profiling_db -name '*json' -exec cat {} \; | jq -s '[.[] | { command, timestamp, exit_status, elapsed_time_sec, max_res_set_sz_kb, stdout }] | sort_by(.timestamp)' |less
